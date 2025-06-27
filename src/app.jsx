9d65b977d0b6208a8da625afac75302e096cc6f0
Daniel-1600 <100189314+Daniel-1600@users.noreply.github.com>
2025-06-27 16:22:57 +0300

    the about section

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
 import Cocktails from "./components/cocktails";
+import About from "./components/about";
 
 const app = () => {
   return (
@@ -9,6 +10,7 @@ const app = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 };
